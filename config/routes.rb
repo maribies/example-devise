a8161e790ab2088d58756a2c0cc2cf8f6e8e5702
Marissa Biescker <marissa.biesecker@gmail.com>
2022-01-09 00:18:03 +0000

    Add edit password to user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    registrations: 'registrations',
+  }
   root to: "home#index"
+
+  get 'users/show', to: 'users#show', as: 'user_root'
 end
